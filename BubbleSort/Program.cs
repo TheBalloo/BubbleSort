8efbcf02ecb864decd488b79861d867900cb4ab3
chursyatina <maksim.mohkin@mail.ru>
2025-03-20 23:15:24 +0300

    really added bad code

---
+++ BubbleSort/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] nums = { 1, 5, 45, 8, 7, 45, 85, 44, 37 };
+
+Random rand = new Random();
+int temp;
+for (int x = 0; x < nums.Length - 1; x++)
+{
+    for (int y = x + 1; y < nums.Length; y++)
+    {
+        if (nums[x] > nums[y])
+        {
+            temp = nums[x];
+            nums[x] = nums[y];
+            nums[y] = temp;
+        }
+    }
+    Console.WriteLine(nums[x]);
+}
