6c560536ee2abe28e69ca14e417bfc1d91c16c6a
TheBalloo <legend.037@yandex.ru>
2025-03-23 11:17:24 +0300

    Исправил как смог но цикл начинается сначала с уже введеными данными

--- BubbleSort/Program.cs
+++ BubbleSort/Program.cs
@@ -1,35 +1,4 @@
-﻿//Random rand = new Random();
-//int length =  rand.Next(0, 100);
-//int[] nums = new int [length];
-//int[] numsCopy = new int[length];
-//int some = 0;   
-//for (int y = 0;y < length;y++)
-//{
-//    some = rand.Next(0, 1000);
-//    nums[y] = some;
-//    numsCopy[y] = some;
-
-//}
-
-//for (int i = 0; i < nums.Length; i++)
-//{
-//    nums[i] = rand.Next(0, 100);
-//}
-
-//int temp;
-//for (int x = 0; x < nums.Length - 1; x++)
-//{
-//    for (int y = x + 1; y < nums.Length; y++)
-//    {
-//        if (nums[x] > nums[y])
-//        {
-//            temp = nums[x];
-//            nums[x] = nums[y];
-//            nums[y] = temp;
-//        }
-//    }
-//    Console.WriteLine(nums[x]);
-//}
+﻿
 
 
 
@@ -40,33 +9,64 @@ double result;
 char oper;
 Console.WriteLine("Введите первое число");
 num1 = Convert.ToDouble(Console.ReadLine());
-Console.WriteLine("Введите операцию число");
+Console.WriteLine("Введите  операцию");
 oper = Convert.ToChar(Console.ReadLine());
 Console.WriteLine("Введите второе число число");
 num2 = Convert.ToDouble(Console.ReadLine());
-
-if (oper == + '+')
+bool Calculate = true;
+while (Calculate)
 {
-    result = num1 + num2;
-    Console.WriteLine("Cумма " + num1 + " и " + num2 + " равна " + result + ".");
+    switch (oper)
+    {
+        case '+':
+            result = num1 + num2;
+            Calculate = false;
+            Console.WriteLine("Cумма " + num1 + " и " + num2 + " равна " + result + ".");
+            break;
 
-}
-else if (oper == '-')
-{
-    result = num1 - num2;
-    Console.WriteLine("Разность " + num1 + " и " + num2 + " равна " + result + ".");
-}
-else if (oper == '/')
+        case '-':
+            result = num1 - num2;
+            Console.WriteLine("Разность " + num1 + " и " + num2 + " равна " + result + ".");
+            break;
+
+        case '*':
+            result = num1 * num2;
+            Console.WriteLine("Умножение " + num1 + " и " + num2 + " равно " + result + ".");
+            break;
+
+        case '/':
+            if (num2 != 0)
+            {
+                result = num1 / num2;
+                Console.WriteLine("Деление " + num1 + " и " + num2 + " равно " + result + ".");
+            }
+            else
+                Console.WriteLine("Ошибка!  Деление на ноль");
+            
+            break;
+
+        case '%':
+            result = num1 % num2;
+            Console.WriteLine("Остаток от деления " + num1 + " и " + num2 + " равен " + result + ".");
+            break;
+             
+    }
+    Console.WriteLine("Хотите продолжить? (y/n)");
+    char reolad = Console.ReadLine()[0];
+
+    if (reolad == 'n' || reolad == 'N')
     {
-    result = num1 / num2;
-    Console.WriteLine("Деление " + num1 + " и " + num2 + " равно " + result + ".");
-}
-else if (oper == '*')
+        Calculate = false;
+    }
+    else if (reolad == 'y' || reolad == 'Y')
     {
-    result = num1 * num2;
-    Console.WriteLine("Умножение " + num1 + " и " + num2 + " равно " + result + ".");
-}
-else
+        Calculate = true;
+    }
+    else
 {
     Console.WriteLine("Неизвестная операция");
 }
+
+}
+
+
